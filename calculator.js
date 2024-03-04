986e14328649e4e08660e38225ade4ebba6deedd
Ricardo Pena <145997059+Ricardop970326@users.noreply.github.com>
2024-03-04 09:28:58 -0500

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -10,7 +10,7 @@ class Calculator{
      this.previousOperand = ''
      this.operation = ''
     }
-
+/////
     delete() {
        this.currentOperand = this.currentOperand.toString().slice(0, -1);
     }
